3ab5ca5d3c08b3eba5430ccb710adfc0d1048927
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-04-26 17:44:45 -0400

    Get up to speed and create seeds for restaurants.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
 
   before_action :ensure_logged_in, except: [:show, :index]
-  before_action :load_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :load_restaurant, only: [:edit, :update, :destroy, :show]
   before_action :ensure_user_owns_restaurant, only: [:edit, :update, :destroy]
 
   def index
@@ -16,7 +16,10 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @user = User.find(current_user.id)
     @review = Review.new
+    @reservation = Reservation.new
+    @reservations = @user.reservations.where(params[:restaurant_id])
   end
 
   def update
@@ -37,7 +40,8 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant.user_id = user.id
+    @restaurant = Restaurant.new
+    @restaurant.user_id = current_user.id
     @restaurant.name = params[:restaurant][:name]
     @restaurant.address = params[:restaurant][:address]
     @restaurant.capacity = params[:restaurant][:capacity]
@@ -45,7 +49,7 @@ class RestaurantsController < ApplicationController
     @restaurant.summary = params[:restaurant][:summary]
     @restaurant.menu = params[:restaurant][:menu]
     @restaurant.opening_hour = params[:restaurant][:opening_hour]
-    @restaurant.closing_time = params[:restaurant][:cost]
+    @restaurant.closing_hour = params[:restaurant][:closing_hour]
 
     if @restaurant.save
       redirect_to restaurant_url(@restaurant.id)
@@ -54,20 +58,25 @@ class RestaurantsController < ApplicationController
     end
   end
 
+
+
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+
     @restaurant.destroy
 
     redirect_to root_path
   end
 
-  def load_picture
+  def load_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
 
   def ensure_user_owns_restaurant
+    #restaurant = Restaurant.find(params[:id])
     unless current_user.id == @restaurant.user_id
       flash[:alert] = "Please log in"
-      redirect_to new_sessions_url
+      redirect_to restaurant_path(@restaurant)
     end
   end
 end
